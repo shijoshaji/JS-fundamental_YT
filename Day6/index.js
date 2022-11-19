59d22de6056a1628570c19c583ff9ba48f9ea898
Shijo Shaji <jojovms@gmail.com>
2022-11-19 13:59:34 +0530

    done

--- Day6/index.js
+++ Day6/index.js
@@ -5,12 +5,12 @@ console.log('day 6');
 
 const names = ['John', 'Mathew', 'Luke', 'Mark', 'Abraham', 'Issac'];
 const people = [
-  { uName: 'John', role: 'SDE' },
-  { uName: 'Mathew', role: 'SDE2' },
-  { uName: 'Luke', role: 'SDE4' },
-  { uName: 'Mark', role: 'TL' },
-  { uName: 'Abraham', role: 'Principal Staff' },
-  { uName: 'Issac', role: 'MD' },
+  { uName: 'John', role: 'SDE', age: 25, id: 1, salary: 200 },
+  { uName: 'Mathew', role: 'SDE2', age: 25, id: 2, salary: 300 },
+  { uName: 'Luke', role: 'SDE4', age: 30, id: 3, salary: 400 },
+  { uName: 'Mark', role: 'TL', age: 35, id: 4, salary: 600 },
+  { uName: 'Abraham', role: 'Principal Staff', age: 36, id: 5, salary: 800 },
+  { uName: 'Issac', role: 'MD', age: 40, id: 5, salary: 1000 },
 ];
 
 for (let index = 0; index < names.length; index++) {
@@ -39,10 +39,36 @@ people.forEach(function (eachPerson) {
   console.log(eachPerson.uName);
 });
 
-// map - return new array
+// map - return new array - size will be as of array that its derived from
 
 const roles = people.map(function (item) {
   return item.role;
 });
 
 console.log('New Array:', roles);
+
+// filter - return new array based on condition
+
+const youngForce = people.filter(function (item) {
+  return item.age <= 30;
+});
+
+console.log('Young ppl', youngForce);
+
+// find - return new aray based on condtion, good for unique values. first matched value; if no match its undefined
+
+const unique = people.find(function (item) {
+  return item.id === 5;
+});
+
+console.log('unique', unique);
+
+// reduce - reduces to single value - number,array,object
+// always return the first parameter
+
+const total = people.reduce(function (acc, cur) {
+  acc += cur.salary;
+  return acc;
+}, 0);
+
+console.log('total', total);
